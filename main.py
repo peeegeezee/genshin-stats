b66623700e7a012852bb4ae3014592bca3c137ce
Marie <marie@kaifa.ch>
2022-07-02 16:06:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import asyncio
 import logging
 import os
 import pathlib
+import time
 
 import genshin
 import jinja2
+import requests
+from bs4 import BeautifulSoup
 
 logger = logging.getLogger()
 
@@ -44,6 +47,45 @@ async def main():
         reward_info=reward_info,
     )
     args.output.write_text(rendered)
+    
+    #%% Get new codes
+
+    res = requests.get("https://www.pockettactics.com/genshin-impact/codes")
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    active_codes = [code.text.strip() for code in soup.find("div", {"class":"entry-content"}).find("ul").findAll("strong")]
+
+    codes_file = root / "codes.txt"
+    used_codes = codes_file.open().read().split("\n")
+    new_codes = list(filter(lambda x: x not in used_codes and x != "", active_codes))
+    
+    #%% Redeem new codes
+
+    print("[Code redeem] ", end="")
+    failed_codes = []
+    for code in new_codes[:-1]:
+        try:
+            await client.redeem_code(code)
+        except Exception as e:
+            failed_codes.append(code)
+        time.sleep(5.2)
+    if len(new_codes) != 0:
+        try:
+            await client.redeem_code(new_codes[-1])
+        except Exception as e:
+            failed_codes.append(new_codes[-1])
+
+    redeemed_codes = list(filter(lambda x: x not in failed_codes, new_codes))
+    if len(redeemed_codes) != 0:
+        print("Redeemed " + str(len(redeemed_codes)) + " new codes: " + ", ".join(redeemed_codes))
+    else:
+        print("No new codes found")
+
+
+    #%% Add new codes to used codes
+
+    used_codes.extend(new_codes)
+    io.open(codes_file, "w", newline="\n").write("\n".join(used_codes))
 
 
 if __name__ == "__main__":
